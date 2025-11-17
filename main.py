2577b5e78da402f201610af5f224a1188e0a20ab
Red <Red123@mail.ru>
2025-11-17 13:57:51 +0300

    22

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
-import main
+import speech_recognition
 
-sr = main.Recognizer()
-with main.Microphone() as mic:
+sr = speech_recognition.Recognizer()
+with speech_recognition.Microphone() as mic:
     sr.adjust_for_ambient_noise(source=mic, duration=0.7)
     audio = sr.listen(source=mic)
     query = sr.recognize_google(audio_data= audio, language='ru-RU').lower()
