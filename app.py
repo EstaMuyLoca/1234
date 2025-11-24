9d77c5963c6163fa2a4a4ecb647c9d53a4f638fb
EstaMuyLoca <qwertasd123142a2mail.ru>
2025-11-24 21:45:12 +0300

    s

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sounddevice as sd
 import vosk
 import json
 import words 
-import skills  
+from skills import * 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 q = queue.Queue()
