e4785a4108211e18e7a255d9bdca6ad02d6acb8b
EstaMuyLoca <qwertasd123142a2mail.ru>
2025-11-25 16:06:16 +0300

    4

--- commands.py
+++ commands.py
@@ -1,10 +1,11 @@
 import os, webbrowser, sys, requests, subprocess, voice
 import pyttsx3
-
 engine = pyttsx3.init()
 engine.setProperty('rate', 180)
 
+
 def speaker(text):
+
     engine.say(text)
     engine.runAndWait()
 
